46e2849702d1ffac549f028853570247968ef540
Girish Gokul <girishg1192@gmail.com>
2016-04-01 02:50:22 -0400

    Queryall impl

--- app/src/main/java/edu/buffalo/cse/cse486586/simpledht/SimpleDhtProvider.java
+++ app/src/main/java/edu/buffalo/cse/cse486586/simpledht/SimpleDhtProvider.java
@@ -1,6 +1,7 @@
 package edu.buffalo.cse.cse486586.simpledht;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -36,12 +37,15 @@ public class SimpleDhtProvider extends ContentProvider {
     private String nodeID;
     public static String TAG = "DHTProvider";
     static Context mContext = null;
-    public static String JOIN_REQUEST = "JOINNOW!";
+
+    private static String JOIN_REQUEST = "JOINNOW!";
     private static String JOIN_ACCEPT = "Joined";
     private static String PREV_UPDATE = "Prev_Update";
     private static String INSERT_KEY = "InsertKey";
     private static String QUERY_KEY = "QueryKey";
     private static String QUERY_RESULT = "QueryResult";
+    private static String QUERY_ALL = "QueryAll";
+
     private static String delim = "`";
     Semaphore waitForQuery = new Semaphore(0);
     boolean flag = false;
@@ -65,7 +69,6 @@ public class SimpleDhtProvider extends ContentProvider {
 
     @Override
     public Uri insert(Uri uri, ContentValues values) {
-
         Set<Map.Entry<String, Object>> contentSet = values.valueSet();
         String args[] = new String[2];  //args[0] value, args[1] key
         Iterator start = contentSet.iterator();
@@ -124,31 +127,43 @@ public class SimpleDhtProvider extends ContentProvider {
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
                         String sortOrder) {
 
-        String queryDht = QUERY_KEY + delim + selection + delim + myPort;
-//        new ClientTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, queryDht, String.valueOf(myPort));
-        sendMessage(queryDht, String.valueOf(myPort));
-        Log.v("Query Result", queryDht);
-
-        try {
-            waitForQuery.acquire();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        MatrixHelper cursorBuilder = null;
+        if (selection.equals("@")) {
+            Log.v("DHTQuery", "Local dump");
+            cursorBuilder = new MatrixHelper(localQueryAll());
+        } else if (selection.equals("*")){
+            String queryAllDht = QUERY_ALL + delim + myPort;
+            sendMessage(queryAllDht, String.valueOf(myPort));
+            try {
+                waitForQuery.acquire();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            cursorBuilder = new MatrixHelper(queryResult);
         }
-        Log.v("QueryResult", queryResult + " " + selection);
-        if (queryResult == null)
-            return null;
+        else {
+            String queryDht = QUERY_KEY + delim + selection + delim + myPort;
+            sendMessage(queryDht, String.valueOf(myPort));
+            Log.v("Query Result", queryDht);
+            try {
+                waitForQuery.acquire();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Log.v("QueryResult", queryResult + " " + selection);
+            if (queryResult == null)
+                return null;
+            cursorBuilder = new MatrixHelper(selection + delim + queryResult);
+        }
         //TODO add more rows
-        MatrixHelper cursorBuilder = new MatrixHelper(selection + " " + queryResult);
 
         return cursorBuilder.cursor;
     }
 
     @Override
     public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
-        // TODO Auto-generated method stub
         return 0;
     }
-
 
     private String genHash(String input) {
         MessageDigest sha1;
@@ -170,7 +185,6 @@ public class SimpleDhtProvider extends ContentProvider {
         @Override
         protected Void doInBackground(ServerSocket... sockets) {
             ServerSocket serverSocket = sockets[0];
-//            Log.e(TAG, "In here! waiting for a client");
             do {
                 try {
                     Socket clientHook = serverSocket.accept();
@@ -190,13 +204,15 @@ public class SimpleDhtProvider extends ContentProvider {
                         handleInsert(args[1], args[2]);
                     } else if (args[0].equals(QUERY_KEY)) {
                         queryKey(args[1], args[2]);
+                    } else if (args[0].equals(QUERY_ALL)){
+                        queryAll(message);
                     } else if (args[0].equals(QUERY_RESULT)) {
-                        queryResult = args[1];
+                        message = message.substring(message.indexOf(delim)+1);
+                        queryResult = message;
                         waitForQuery.release();
                         flag = true;
                     }
                     //TODO received some message change to serializable object?
-
                     clientHook.close();
                     //serverSocket.close();
                 } catch (IOException e) {
@@ -211,37 +227,49 @@ public class SimpleDhtProvider extends ContentProvider {
         }
     }
 
+    private void queryAll(String message) {
+        String[] args = message.split(delim);
+        if(nextNode == prevNode){
+            String result = QUERY_RESULT + delim + localQueryAll();
+            sendMessage(result, args[1]);
+            return;
+        }
+        String localValues = localQueryAll();
+
+        if(localValues!=null)
+           message = message + delim + localValues.substring(0, localValues.lastIndexOf(delim));
+
+        if(nextNode == Integer.parseInt(args[1])) {
+            int cutString = message.indexOf(delim, message.indexOf(delim)+1);
+            message = QUERY_RESULT + message.substring(cutString);
+        }
+        sendMessage(message, String.valueOf(nextNode));
+    }
+
     private void queryKey(String key, String port) {
         String hash = genHash(key);
-        String nextID = genHash(String.valueOf(nextNode / 2));
-        if (greaterThan(hash, nodeID) && !greaterThan(hash, nextID)) {
-            Log.v("Query", "Query found");
-
+        if (nextNode == prevNode) {
             localQuery(key, port);
-        }/*else if ((greaterThan(hash, nodeID) &&
-                greaterThan(hash, nextID)) || !greaterThan(hash,nodeID)) {
-            if (greaterThan(nextID, nodeID)) {
-                String insDht = INSERT_KEY + delim + key + delim + value;
-                new ClientTask().executeOnExecutor(AsyncTask.SERIAL_EXECUTOR, insDht, String.valueOf(nextNode));
-            } else {
-                Log.v("Insert", "err");
-                localInsert(key, value);
-            }
-        }*/ else if (!greaterThan(nextID, nodeID)) {
-            if (greaterThan(hash, nodeID) || !greaterThan(hash, nextID)) {
+            return;
+        }
+//        String nextID = genHash(String.valueOf(nextNode/2));
+        String prevID = genHash(String.valueOf(prevNode / 2));
+        if (greaterThan(hash, prevID) && !greaterThan(hash, nodeID)) {
+            Log.v("Query", "Query found");
+            localQuery(key, port);
+        } else if (!greaterThan(nodeID, prevID)) {
+            if ((!greaterThan(hash, nodeID) && greaterThan(prevID, hash))
+                    || (greaterThan(hash, prevID)) && greaterThan(hash, nodeID)) {
                 //local query
                 localQuery(key, port);
             } else {
                 String insDht = QUERY_KEY + delim + key + delim + port;
-//                new ClientTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, insDht, String.valueOf(nextNode));
                 sendMessage(insDht, String.valueOf(nextNode));
             }
         } else {
             String insDht = QUERY_KEY + delim + key + delim + port;
-//            new ClientTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, insDht, String.valueOf(nextNode));
             sendMessage(insDht, String.valueOf(nextNode));
         }
-
     }
 
     private void localQuery(String key, String port) {
@@ -257,7 +285,6 @@ public class SimpleDhtProvider extends ContentProvider {
             }
             Log.v("query", "key " + key + " value " + message);
             String queryResult = QUERY_RESULT + delim + message;
-//            new ClientTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, queryResult, String.valueOf(port));
             sendMessage(queryResult, String.valueOf(port));
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -266,24 +293,49 @@ public class SimpleDhtProvider extends ContentProvider {
         }
     }
 
+    private String localQueryAll() {
+        File dir = getContext().getFilesDir();
+        String[] files = dir.list();
+        String queryAll = null;
+        FileInputStream key_retrieve;
+        String message;
+
+        for (String file : files) {
+            Log.v("LocalQuery", file);
+            try {
+                key_retrieve = getContext().openFileInput(file);
+                if (key_retrieve == null)
+                    message = null;
+                else {
+                    BufferedReader buf = new BufferedReader(new InputStreamReader(key_retrieve));
+                    message = buf.readLine();
+//                    queryAll.addRow(file, message);
+                    if(queryAll == null)
+                        queryAll = file + delim + message + delim;
+                    else
+                        queryAll = queryAll + file + delim + message + delim;
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+//        Log.v("QueryAll", queryAll);
+        return queryAll;
+    }
+
     private void handleInsert(String key, String value) {
         String hash = genHash(key);
-        String nextID = genHash(String.valueOf(nextNode / 2));
+//        String nextID = genHash(String.valueOf(nextNode / 2));
         String prevID = genHash(String.valueOf(prevNode / 2));
-        if (greaterThan(hash, nodeID) && !greaterThan(hash, nextID)) {
+        if (greaterThan(hash, prevID) && !greaterThan(hash, nodeID)) {
             Log.v("Insert", "Insert in node");
             localInsert(key, value);
-        }/*else if ((greaterThan(hash, nodeID) &&
-                greaterThan(hash, nextID)) || !greaterThan(hash,nodeID)) {
-            if (greaterThan(nextID, nodeID)) {
-                String insDht = INSERT_KEY + delim + key + delim + value;
-                new ClientTask().executeOnExecutor(AsyncTask.SERIAL_EXECUTOR, insDht, String.valueOf(nextNode));
-            } else {
-                Log.v("Insert", "err");
-                localInsert(key, value);
-            }
-        }*/ else if (!greaterThan(nextID, nodeID)) {
-            if (greaterThan(hash, nodeID) || !greaterThan(hash, nextID)) {
+        } else if (!greaterThan(nodeID, prevID)) {
+            //Double negatives!!
+            if ((!greaterThan(hash, nodeID) && greaterThan(prevID, hash))
+                    || (greaterThan(hash, prevID)) && greaterThan(hash, nodeID)) {
                 localInsert(key, value);
             } else {
                 String insDht = INSERT_KEY + delim + key + delim + value;
@@ -378,7 +430,6 @@ public class SimpleDhtProvider extends ContentProvider {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return;
     }
 
     private boolean greaterThan(String first, String second) {
